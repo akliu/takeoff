2db2bad0145f8ccdd940df3f8ae5c012be37fdb8
Andrew Liu <andrew.k.liu89@gmail.com>
2015-10-14 10:53:59 -0700

    Sidebar and styling updated

--- app/assets/javascripts/components/sidebar.js.jsx
+++ app/assets/javascripts/components/sidebar.js.jsx
@@ -5,8 +5,8 @@
 
     render: function(){
       return (
-        <nav className="sidebar-nav">
-          <ul className="">
+        <nav className="navbar sidebar-nav navbar-default">
+          <ul className="nav in">
             <li><a href="#">View Reservations</a></li>
             <li><a href="#">Previous Trips</a></li>
             <li><a href="#">Register My Jet</a></li>
