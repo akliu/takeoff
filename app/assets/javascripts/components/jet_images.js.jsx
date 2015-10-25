429ed36e28fca90fbaf3a5a7cadaa0c4589aa57d
Andrew Liu <andrew.k.liu89@gmail.com>
2015-10-25 01:13:27 -0700

    updated jet images

--- app/assets/javascripts/components/jet_images.js.jsx
+++ app/assets/javascripts/components/jet_images.js.jsx
@@ -6,19 +6,23 @@
     [
     "https://res.cloudinary.com/dx1lykanb/image/upload/v1445210901/gulfstream_450/gulfstream_450_1.jpg",
     "https://res.cloudinary.com/dx1lykanb/image/upload/v1445210901/gulfstream_450/gulfstream_450_2.jpg",
-    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445208108/sample.jpg"
+    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445750947/gulfstream_450/gulfstream_450_3.jpg"
     ],
 
     "Gulfstream 550":
     [
     "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220458/gulfstream_550/gulfstream_550_1.jpg",
-    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220458/gulfstream_550/gulfstream_550_2.jpg"
+    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220458/gulfstream_550/gulfstream_550_2.jpg",
+    "http://res.cloudinary.com/dx1lykanb/image/upload/c_scale,w_1200/v1445751118/gulfstream_550/gulfstream_550_3.jpg"
+
     ],
 
     "Gulfstream 650":
     [
     "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220699/gulfstream_650/gulfstream_650_1.jpg",
-    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220699/gulfstream_650/gulfstream_650_2.jpg"
+    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445220699/gulfstream_650/gulfstream_650_2.jpg",
+    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445751392/gulfstream_650/gulfstream_650_3.jpg",
+    "https://res.cloudinary.com/dx1lykanb/image/upload/v1445751393/gulfstream_650/gulfstream_650_4.jpg"
     ]
   };
 
